4972992f57c8302cf0f113eb0116a399691c8e4c
John Nilsen <nilsen340@gmail.com>
2016-02-04 14:18:11 +0100

    Up SDK version to 1.1.2

--- tapglue-android-sdk/src/main/java/com/tapglue/networking/TGNetworkManager.java
+++ tapglue-android-sdk/src/main/java/com/tapglue/networking/TGNetworkManager.java
@@ -96,7 +96,7 @@ public class TGNetworkManager {
      * Current library version - string used in requests
      */
     @NonNull
-    private static final String currentLibraryVersion = "1.1.1";
+    private static final String currentLibraryVersion = "1.1.2";
 
     /**
      * Custom headers required for analytics purposes
