88735966b24c5ace5eca2f9354bb069875a4e465
John Nilsen <nilsen340@gmail.com>
2016-07-28 14:34:22 +0200

    Change example user to pablo (for sims reasons)

--- app/src/main/java/com/tapglue/example/MainActivity.java
+++ app/src/main/java/com/tapglue/example/MainActivity.java
@@ -38,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
         Configuration configuration = new Configuration("https://api.tapglue.com", "2da2d79336c2773c630ce46f5d24cb76");
         configuration.setLogging(true);
         final RxTapglue tapglue = new RxTapglue(configuration, this);
-        User user = new User("testUser000001", "password");
+        User user = new User("pablo", PasswordHasher.hashPassword("supersecret"));
         tapglue.createUser(user).subscribeOn(Schedulers.io()).subscribe(new Observer<User>() {
             @Override
             public void onCompleted() {
-                tapglue.loginWithUsername("testUser000001", "password").subscribe();
+                tapglue.loginWithUsername("pablo", PasswordHasher.hashPassword("supersecret")).subscribe();
             }
 
             @Override
