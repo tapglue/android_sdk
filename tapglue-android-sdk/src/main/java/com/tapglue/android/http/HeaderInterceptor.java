b2f8a1ccd9c6b5de39a837d937e1ca1f0cbcd9af
John Nilsen <nilsen340@gmail.com>
2016-11-30 12:51:28 +0100

    Up version to 3.0.0-alpha-4

--- tapglue-android-sdk/src/main/java/com/tapglue/android/http/HeaderInterceptor.java
+++ tapglue-android-sdk/src/main/java/com/tapglue/android/http/HeaderInterceptor.java
@@ -28,7 +28,7 @@ import okhttp3.Response;
 
 class HeaderInterceptor implements Interceptor {
 
-    private static final String VERSION = "3.0.0-alpha-3";
+    private static final String VERSION = "3.0.0-alpha-4";
     String appToken;
     String sessionToken;
     String uuid;
